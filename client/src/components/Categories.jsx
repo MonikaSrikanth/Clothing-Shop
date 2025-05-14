fda71ffdf7240bbee05a8bd761907ebc88f00649
MonikaSrikanth <monikasubha3@gmail.com>
2025-05-14 12:39:16 +0530

    Scroll problem fixed

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,13 +5,16 @@ const Categories = () => {
     { name: "Women", image: "/women.jpeg",path:"/women" },
     { name: "Kids", image: "/kid.jpeg",path:"/kids" },
   ];
+  const handleCategoryClick = () => {
+    window.scrollTo(0, 0);  // Scroll to top
+  };
 
   return (
     <section id="3" className="py-16 bg-gray-100">
       <h2 className="text-3xl font-bold text-center">Shop by Category</h2>
       <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6">
         {categories.map((cat, index) => (
-          <Link to={cat.path} key={index} className="relative group block">
+          <Link to={cat.path} key={index} className="relative group block"onClick={handleCategoryClick}>
             <img
               src={cat.image}
               alt={cat.name}
